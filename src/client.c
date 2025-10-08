a9278947898c90328b8317868daed65efb122eb3
Kabuñiang Buhawi (KB) Monjardin <82626679+Dowiw@users.noreply.github.com>
2025-10-08 21:33:43 +0200

    add: use libft
    Refactor client.c to use ft_atoi and remove prints.

--- src/client.c
+++ src/client.c
@@ -1,7 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmonjard <kmonjard@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 20:42:39 by kmonjard          #+#    #+#             */
+/*   Updated: 2025/10/08 20:42:40 by kmonjard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 #include <signal.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 void	char_to_sig(pid_t *pid, char c)
 {
@@ -12,23 +23,18 @@ void	char_to_sig(pid_t *pid, char c)
 	{
 		if ((c >> i) & 1)
 		{
-			write(1, "1", 1);
 			kill(*pid, SIGUSR1);
 		}
 		else
 		{
-			write(1, "0", 1);
 			kill(*pid, SIGUSR2);
 		}
 		usleep(500);
 	}
-	write(1, "\n", 1);
-	usleep(100);
 }
 
 void	str_to_sig(pid_t *pid, char *str)
 {
-	printf("str: %s, pid: %i\n", str, *pid);
 	while (*str)
 	{
 		char_to_sig(pid, *str);
@@ -45,14 +51,14 @@ void	str_to_sig(pid_t *pid, char *str)
  * @param av[1] as server_pid, av[2] as string
  * @param ac must be 3 args
  */
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	char	*msg;
 	pid_t	server_pid;
 
 	if (ac != 3)
 		return (write(2, "Usage: ./client <server_pid> <string>\n", 38), 1);
-	server_pid = atoi(av[1]);
+	server_pid = ft_atoi(av[1]);
 	msg = av[2];
 	str_to_sig(&server_pid, msg);
 	return (0);
